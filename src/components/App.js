351b003b498230c3a04835352858d8f829457f3e
Lokesh Baviskar <101720181+22karlokesh@users.noreply.github.com>
2025-07-06 13:13:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,79 @@
 
-import React from "react";
+// import React from "react";
+// import './../styles/App.css';
+
+// const App = () => {
+//   return (
+//     <div>
+//         {/* Do not remove the main div */}
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+import React, { useState } from "react";
+import axios from "axios";
 import './../styles/App.css';
 
 const App = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [fetched, setFetched] = useState(false);
+
+  const fetchUsers = () => {
+    setLoading(true);
+    setFetched(false);
+    axios.get("https://reqres.in/api/users")
+      .then(res => {
+        setUsers(res.data.data || []);
+        setLoading(false);
+        setFetched(true);
+      })
+      .catch(() => {
+        setUsers([]);
+        setLoading(false);
+        setFetched(true);
+      });
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      <button className="btn" onClick={fetchUsers}>Get User List</button>
 
-export default App
+      {loading && <p>Loading...</p>}
+
+      {!loading && fetched && users.length === 0 && (
+        <p>No users found.</p>
+      )}
+
+      {!loading && users.length > 0 && (
+        <table border="1" cellPadding="10">
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Avatar</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(user => (
+              <tr key={user.id}>
+                <td>{user.first_name}</td>
+                <td>{user.last_name}</td>
+                <td>{user.email}</td>
+                <td>
+                  <img src={user.avatar} alt={`${user.first_name} avatar`} width="50" />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
+
+export default App;
